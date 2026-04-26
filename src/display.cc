2dbc30b1e4836f17fa4ba0a5058e3f753841f0ec
Joe <joe@localhost>
2026-04-27 00:29:21 +0200

    feat: implement clearRectangle function in display.cc

--- src/display.cc
+++ src/display.cc
@@ -137,3 +137,56 @@ Napi::Boolean clear(const Napi::CallbackInfo& info) {
 
 	return Napi::Boolean::New(env, true);
 }
+
+Napi::Boolean clearRectangle(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+
+    UWORD x1 = (uint32_t)info[0].As<Napi::Number>();
+    UWORD y1 = (uint32_t)info[1].As<Napi::Number>();
+    UWORD x2 = (uint32_t)info[2].As<Napi::Number>();
+    UWORD y2 = (uint32_t)info[3].As<Napi::Number>();
+
+    UWORD color = 0x0000;
+    if (info.Length() > 4) {
+        color = (uint32_t)info[4].As<Napi::Number>() << 8;
+    }
+
+    if (x1 > x2) {
+        UWORD tmp = x1;
+        x1 = x2;
+        x2 = tmp;
+    }
+    if (y1 > y2) {
+        UWORD tmp = y1;
+        y1 = y2;
+        y2 = tmp;
+    }
+
+    if (x1 >= LCD_WIDTH) {
+        printf("clearRectangle: x1 (%d) >= LCD_WIDTH (%d), clamping to 0\n", x1, LCD_WIDTH);
+        x1 = 0;
+    }
+    if (y1 >= LCD_HEIGHT) {
+        printf("clearRectangle: y1 (%d) >= LCD_HEIGHT (%d), clamping to 0\n", y1, LCD_HEIGHT);
+        y1 = 0;
+    }
+    if (x2 >= LCD_WIDTH) {
+        printf("clearRectangle: x2 (%d) >= LCD_WIDTH (%d), clamping to %d\n", x2, LCD_WIDTH, LCD_WIDTH - 1);
+        x2 = LCD_WIDTH - 1;
+    }
+    if (y2 >= LCD_HEIGHT) {
+        printf("clearRectangle: y2 (%d) >= LCD_HEIGHT (%d), clamping to %d\n", y2, LCD_HEIGHT, LCD_HEIGHT - 1);
+        y2 = LCD_HEIGHT - 1;
+    }
+
+    if (x2 < x1 || y2 < y1) {
+        DEBUG("clearRectangle: invalid rectangle after clamping");
+        return Napi::Boolean::New(env, false);
+    }
+
+    Paint_ClearWindow(x1, y1, x2 + 1, y2 + 1, color);
+
+    LCD_1in44_Display(displayBuffer);
+
+    return Napi::Boolean::New(env, true);
+}
